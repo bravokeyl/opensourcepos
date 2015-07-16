cfaed2d40cd8e3c3d0ea3e01e90aef9b6ecba0cf
bravokeyl <bravokeyl@gmail.com>
2015-07-16 11:34:33 +0530

    HTML5 , Change layout width to 1140px

--- application/views/partial/header.php
+++ application/views/partial/header.php
@@ -1,8 +1,7 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html>
 <head>
-	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<meta charset="utf-8" />
 	<base href="<?php echo base_url();?>" />
 	<title><?php echo $this->config->item('company').' -- '.$this->lang->line('common_powered_by').' OS Point Of Sale' ?></title>
 	<link rel="stylesheet" type="text/css" href="css/ospos.css"/>
