f82a944478685427bb3d88d9c44def4d0b3eb40c
bravokeyl <bravokeyl@gmail.com>
2015-07-20 09:58:17 +0530

    Customers don't like to see OSPOS powered

--- application/views/partial/header.php
+++ application/views/partial/header.php
@@ -63,7 +63,6 @@ html {
 			<div id="menubar_container">
 				<div id="menubar_company_info">
 				<span id="company_title"><?php echo $this->config->item('company'); ?></span><br />
-				<span style='font-size:8pt;'><?php echo $this->lang->line('common_powered_by').' Open Source Point Of Sale'; ?></span>
 			</div>
 			<div id="menubar_navigation" class="nav navbar-right">
 				<?php
